add61c97768f760fbaae876179684414a2f68fed
Juls <78077799+princeyayain123@users.noreply.github.com>
2025-08-06 11:47:06 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -179,7 +179,7 @@ function loadModel() {
   loader.setDRACOLoader(dracoLoader);
   loader.setKTX2Loader(ktx2Loader);
   loader.load(
-    "./assets/model/merge-op.glb",
+    "./assets/model/merge.glb",
     (gltf) => {
       model = gltf.scene;
       model.scale.set(2.5, 2.5, 2.5);
