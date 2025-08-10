cee8a3ad97b798205923cc27dd27bf15efdf98c2
Juls <78077799+princeyayain123@users.noreply.github.com>
2025-08-10 10:48:18 +0800

    Update pdfSignature.js

--- js/pdfSignature.js
+++ js/pdfSignature.js
@@ -14,6 +14,7 @@ const startPDFApp = () => {
     generatePDF();
     checkInputs();
     fetchAPI();
+    await sendData();
   }
 
   async function fetchAPI() {
@@ -141,7 +142,7 @@ const startPDFApp = () => {
 
   async function sendData() {
     await startSession();
-    await uploadPDFToBackend(file);
+    // await uploadPDFToBackend(file);
   }
 
   function addEvent() {
@@ -346,8 +347,6 @@ const startPDFApp = () => {
 
       const file = new File([blob], "Pompanette_Boat_Seat_Configuration_Agreement.pdf", { type: "application/pdf" });
       loadingAnimation();
-
-      await sendData();
     });
   }
 
